4beddc41c90f70a7620db11eee8a7f68ac5ad373
RoomyPatel <roomyp.mobio@gmail.com>
2021-09-13 19:49:48 +0530

    first commit

---
+++ Template/assets/js/script.js
@@ -0,0 +1,11 @@
+// Navigation Section Start  
+$(document).ready(function() {
+    $('.navbar-toggler').on('click', function() {
+        $('.animated-icon').toggleClass('open');
+    });
+});
+// Navigation Section End  
+
+// $(document).ready(function(){
+//     $('.dropdown-toggle').dropdown();
+// });
