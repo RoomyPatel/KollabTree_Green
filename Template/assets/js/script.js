1c0b2853b89a734d2cab2ab6bd1fdfb71c604d2d
RoomyPatel <roomyp.mobio@gmail.com>
2021-09-16 19:03:36 +0530

    master - changes and sections are added

--- Template/assets/js/script.js
+++ Template/assets/js/script.js
@@ -9,3 +9,20 @@ $(document).ready(function() {
 // $(document).ready(function(){
 //     $('.dropdown-toggle').dropdown();
 // });
+
+// Customers Section Start 
+function openFeed(evt, FeedName) {
+    // Declare all variables
+    var i, tabcontent;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(FeedName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  // Customers Section End 
