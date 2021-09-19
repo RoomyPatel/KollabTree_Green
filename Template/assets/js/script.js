a613f1069da6b15c4cd52b04b108b19629b980cd
RoomyPatel <roomyp.mobio@gmail.com>
2021-09-19 18:26:55 +0530

    master - changes and new sections are added

--- Template/assets/js/script.js
+++ Template/assets/js/script.js
@@ -6,9 +6,41 @@ $(document).ready(function() {
 });
 // Navigation Section End  
 
-// $(document).ready(function(){
-//     $('.dropdown-toggle').dropdown();
-// });
+$('.slider').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  responsive: [{
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+          }
+      },
+      {
+          breakpoint: 600,
+          settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+          }
+      },
+      {
+          breakpoint: 480,
+          settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+          }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+  ]
+});
+
 
 // Customers Section Start 
 function openFeed(evt, FeedName) {
